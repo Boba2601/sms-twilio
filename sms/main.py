0b9ed505e804d03c9120ac0706bb2ab5a8c4593f
Boba2601 <69029263+Boba2601@users.noreply.github.com>
2022-09-21 22:19:16 +0300

    Create main.py

---
+++ sms/main.py
@@ -0,0 +1,26 @@
+from twilio.rest import Client
+
+
+def twilio_sms(text = ''):
+    try:
+        account_sid = ''
+        auth_token = ''
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+        body = text,
+        from_ = input('Номер Twillo: '),
+        to = input('Номер получателя: ')
+        )
+        return 'Сообщение успешно отправлено!'
+    except Exception as ex:
+        return 'Что-то пошло не так...', ex
+        
+def main():
+    text = input('Ведите свое сообщение: ')
+    print(twilio_sms(text=text))
+   
+    
+
+if __name__ == '__main__':
+    main()
